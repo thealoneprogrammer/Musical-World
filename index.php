0336ea7aa6c6d6e2b29d1629b1ddc098da042a75
Subramanya c <subramanyarao4@gmail.com>
2018-12-26 20:46:53 +0530

    added twak(a chat bot)

--- index.php
+++ index.php
@@ -28,7 +28,19 @@
 	<link href="//fonts.googleapis.com/css?family=Ubuntu:300,300i,400,400i,500,500i,700,700i" rel="stylesheet">
 	<!--//webfonts-->
 </head>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5c2396fb7a79fc1bddf24b28/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 <body>
 	<!-- header -->
 	<header>
