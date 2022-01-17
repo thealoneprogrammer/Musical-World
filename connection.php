c3d94f872cba7a384c78645b799c0873191dbb0c
azizur013 <59233170+azizur013@users.noreply.github.com>
2022-01-17 14:58:47 +0600

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = mysqli_connect('localhost','root','','musical_world');
+	$conn = mysqli_connect('localhost','root','','password');
 
 	if(!$conn)
 		die("Error while connecting...!").mysqli_connect_error($conn);
